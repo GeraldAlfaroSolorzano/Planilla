8e23a7ae596f13d23bc4ca2ce8eaa65dbd580906
Student <Student@LIADSGPC2042>
2025-08-04 16:01:22 -0600

    creacion de almacenamiento puestos y el frame principal

---
+++ src/datos/AlmacenamientoPuestos.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package datos;
+
+import java.util.ArrayList;
+import logica.Puestos;
+
+/**
+ *
+ * @author Student
+ */
+public class AlmacenamientoPuestos {
+    private ArrayList<Puestos> listaPuestos;
+
+    public AlmacenamientoPuestos() {
+        this.listaPuestos = new ArrayList();
+    }
+
+    public ArrayList<Puestos> getListaPuestos() {
+        return listaPuestos;
+    }
+
+    public void setListaPuestos(ArrayList<Puestos> listaPuestos) {
+        this.listaPuestos = listaPuestos;
+    }
+    
+    //Metodos del CRUD
+    
+    public void insertarPuesto(Puestos puesto){
+        if (this.listaPuestos != null){
+            this.listaPuestos.add(puesto);
+        }
+    }
+    
+    public void editarPuesto(int index, Puestos newPuesto){
+        if (index >= 0 && newPuesto != null && !listaPuestos.isEmpty()){
+            this.listaPuestos.set(index, newPuesto);
+        }
+    }
+    
+    public boolean eliminarPuesto(Puestos puesto){
+        if (this.listaPuestos.contains(puesto)){
+            this.listaPuestos.remove(puesto);
+            return true;
+        }
+        return false; //El puesto no existe en la lista 
+    }
+}
